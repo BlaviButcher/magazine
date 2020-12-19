a0673e9109c9606703913445a2e9ddf192cd06c4
blavibutcher <rileydouglas@outlook.com>
2020-12-19 18:33:00 +1300

    Allowed access to variables.

--- magazine.go
+++ magazine.go
@@ -4,26 +4,26 @@ import "fmt"
 
 // Subscriber hold a name string a rate float64 and an active boolean
 type Subscriber struct {
-	name   string
-	rate   float64
-	active bool
+	Name   string
+	Rate   float64
+	Active bool
 }
 
 // PrintInfo will display all info of the subscriber on seperate lines.
 func PrintInfo(s *Subscriber) {
-	fmt.Printf("Name: %s\n", s.name)
-	fmt.Printf("Rate: %0.2f\n", s.rate)
-	fmt.Printf("Active: %t\n", s.active)
+	fmt.Printf("Name: %s\n", s.Name)
+	fmt.Printf("Rate: %0.2f\n", s.Rate)
+	fmt.Printf("Active: %t\n", s.Active)
 }
 // DefaultSubscriber takes a string name and creates a subscriber struct using that name and default everything else.
 func DefaultSubscriber(name string) *Subscriber {
 	var s Subscriber
-	s.name = name
-	s.rate = 5.99
-	s.active = true
+	s.Name = name
+	s.Rate = 5.99
+	s.Active = true
 	return &s
 }
 // ApplyDiscount takes a subscriber and discounts by a dollar.
 func ApplyDiscount(s *Subscriber) {
-	s.rate = 4.99
+	s.Rate = 4.99
 }
